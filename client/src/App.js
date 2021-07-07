385e3801a5a937630fa18966f5c3feaf0d79cef3
DuyetVu2001 <vungocduyet24@gmail.com>
2021-07-07 13:25:08 +0700

    add Login compoent

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import './App.scss';
 import Header from './containers/Header';
+import Login from './components/Login';
 
 function App() {
 	return (
 		<div className="app">
 			<Header />
+			<Login />
 		</div>
 	);
 }
